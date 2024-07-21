f163ff7254773da1d6f89ea54ef50798007f25e6
Khang Trần <148173085+kang0408@users.noreply.github.com>
2024-07-21 15:41:12 +0700

    feat: add settings (#4)

---
+++ src/components/Settings.vue
@@ -0,0 +1,14 @@
+<script setup>
+import settingsvg from '@assets/setting.svg';
+</script>
+
+<template>
+  <div>
+    <h1>Cài đặt</h1>
+    <div
+      class="absolute bottom-0 right-0 left-0 shadow-[5px_10px_10px_0px_rgba(0, 0, 0, 0.25)] shadow-in"
+    >
+      <img :src="settingsvg" alt="" class="w-full" />
+    </div>
+  </div>
+</template>
